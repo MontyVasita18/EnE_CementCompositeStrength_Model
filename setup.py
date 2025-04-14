6a2d4eed9f4819063a068e8a6fff86feb01c9a56
MontyVasita18 <montyvasita143@gmail.com>
2025-04-14 17:26:28 +0530

    project done

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='Machine Learning Model',
     version='0.1.0',
-    author='Chandan Chaudhari',
-    author_email='chaudhari.chandan22@gmail.com',
+    author='Monty Vasita',
+    author_email='montyvasita143@gmail.com',
     description='An MLOps-ready package for Cement Composite Strength Prediction using ML',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
